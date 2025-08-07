0ab4a07ddb794fb81ea7630363aa9157278d819f
keshab Aryal <keshabaryal152@gmail.com>
2025-08-07 12:47:42 +0545

    Add designed navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar-container">
+        <div className="logo">
+          <Link to="/">My Notes</Link>
+        </div>
+        <div className="nav-links">
+          {/* <Link to="/dashboard">Dashboard</Link>
+          <Link to="/profile">Profile</Link>
+          <button>Logout</button> */}
+
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
